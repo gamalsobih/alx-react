9fb71a684143221a65eb52d0f38018d5163b7046
gamalsobih <gamalsob@gmail.com>
2024-08-20 00:55:44 +0000

    ttT

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,10 @@
+// Import jQuery
+import $ from 'jquery';
+
+// Add three different paragraphs to the page body
+$(document).ready(function() {
+    $('body').append('<p>This is the first paragraph.</p>');
+    $('body').append('<p>This is the second paragraph.</p>');
+    $('body').append('<p>This is the third paragraph.</p>');
+});
+
